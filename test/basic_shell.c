54dec6e9e7faf07d66a5e3ae772eebc78bd372ce
Heimer <732@holberton.com>
2019-04-04 19:58:17 +0000

    [feature] test folder

---
+++ test/basic_shell.c
@@ -0,0 +1,39 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+int main(void)
+{
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t read;
+	pid_t child;
+	char *argv[2];
+
+	printf("Ghost-in-the-shell-$ ");
+	while ((read = getline(&line, &len, stdin)) != -1) 
+	{
+			argv[0] = strtok(line, "\n");
+			argv[1] = NULL;	
+	//		printf("%s", line);
+			child = fork();
+			if (!child)
+			{
+    				if (execve(argv[0], argv, NULL) == -1)
+   				 {
+   				     perror("./shell");
+   				 }
+			}
+			else
+			{
+			wait(NULL);
+			}
+	printf("Ghost-in-the-shell-$ ");
+	}
+	free(line);
+	exit(97);
+}
+
